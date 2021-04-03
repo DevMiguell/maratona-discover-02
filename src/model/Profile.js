e336ae4761cb96dc2fce5947e3d0f5e898d66b16
DevMiguell <buguel2000@gmail.com>
2021-04-03 16:36:49 -0300

    editando-profile

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,6 +1,6 @@
 let data = {
-  name: "Mayk",
-  avatar: "https://github.com/maykbrito.png",
+  name: "Miguel",
+  avatar: "https://avatars.githubusercontent.com/u/53983988?s=400&u=74f3c2c08f875bf0aa54d1461c6b697a1d3f4dff&v=4",
   "monthly-budget": 3000,
   "days-per-week": 5,
   "hours-per-day": 5,
